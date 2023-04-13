e90dea01c1658cd2ba63ac148bc256e2c68c7104
RalphHenryDominisac <94041777+RalphHenryDominisac@users.noreply.github.com>
2023-04-14 00:11:52 +0800

    Add files via upload

---
+++ exer7/PROGEXER7 - DOMINISAC, RALPH HENRY L/Prog2Exer7_Dominisac/src/prog2exer1/Prog2Exer1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog2exer1;
+
+/**
+ *
+ * @author RALPH
+ */
+public class Prog2Exer1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CustomerForm abc = new CustomerForm();
+        abc.setVisible(true);
+        
+        secondClass a = new secondClass(); //Instance of secondClass
+       // a.showList();
+       // System.out.print(a.Lists()[0]);
+        
+    }
+    
+}
